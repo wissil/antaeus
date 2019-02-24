6c073a85eb09deabdd211ccf41540f79d228b883
Filip Kozjak <filip.kozjak@asseco-see.hr>
2019-02-24 08:30:17 +0100

    add invoice service tests: fetch invoices with some status

--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
@@ -4,12 +4,25 @@ import io.mockk.every
 import io.mockk.mockk
 import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class InvoiceServiceTest {
+
+    private val pendingInvoices = mockk<List<Invoice>>() {
+        every { isEmpty() } returns false
+    }
+
+    private val paidInvoices = mockk<List<Invoice>>() {
+        every { isEmpty() } returns false
+    }
+
     private val dal = mockk<AntaeusDal> {
         every { fetchInvoice(404) } returns null
+        every { fetchInvoicesWithStatus(InvoiceStatus.PENDING) } returns pendingInvoices
+        every { fetchInvoicesWithStatus(InvoiceStatus.PAID) } returns paidInvoices
     }
 
     private val invoiceService = InvoiceService(dal = dal)
@@ -19,5 +32,11 @@ class InvoiceServiceTest {
         assertThrows<InvoiceNotFoundException> {
             invoiceService.fetch(404)
         }
+    }
+
+    @Test
+    fun `will return non-empty pending and paid invoices`() {
+        assert(invoiceService.fetchAllWithStatus(InvoiceStatus.PENDING).isNotEmpty())
+        assert(invoiceService.fetchAllWithStatus(InvoiceStatus.PAID).isNotEmpty())
     }
 }
